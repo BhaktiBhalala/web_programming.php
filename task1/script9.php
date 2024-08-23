89c4a5fc095003ceeab61819a8648eaa69c8dfef
BhaktiBhalala <bhaktibhalala864@gmail.com>
2024-08-23 07:57:12 +0530

    Add files via upload

---
+++ task1/script9.php
@@ -0,0 +1,10 @@
+<?php
+$name="bhakti";
+$age=20;
+$city="rajkot";
+$mobile=7865787657;
+echo "my name is ".$name."<br>";
+echo "my age is ".$age."<br>";
+echo "my city is ".$city."<br>";
+echo "my mobile no is ".$mobile;
+?>
