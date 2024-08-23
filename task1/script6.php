73ec0d3436cf9495f5da23e1c8d015bfbae071a3
BhaktiBhalala <bhaktibhalala864@gmail.com>
2024-08-23 07:56:27 +0530

    Add files via upload

---
+++ task1/script6.php
@@ -0,0 +1,10 @@
+<?php
+echo "<b>bhakti bhalala</b><br>";
+echo "<i>bhakti bhalala</i><br>";
+echo "<h1>bhakti bhalala</h1><br>";
+echo "<h2>bhakti bhalala</h2><br>";
+echo "<h3>bhakti bhalala</h3><br>";
+echo "<h4>bhakti bhalala</h4><br>";
+echo "<h5>bhakti bhalala</h5><br>";
+echo "<h5>bhakti bhalala</h5><br>";
+?>
